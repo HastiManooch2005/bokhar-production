61dd0a10f669816e58953225165b2ee1df5396fe
hasti2 <hastimanooch84@gmail.com>
2026-07-11 20:57:29 +0330

    add PaymentTerminal in admin.py

--- backend/wallet/admin.py
+++ backend/wallet/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models.models import *
 from .models.setting_payment_models import *
+from .services.refund import Refund
 
 # Register your models here.
 admin.site.register(Wallet)
@@ -9,4 +10,5 @@ admin.site.register(WalletTransaction)
 admin.site.register(WithdrawalRequest)
 admin.site.register(PaymentSession)
 admin.site.register(RefundRequest)
+admin.site.register(PaymentTerminal)
 
